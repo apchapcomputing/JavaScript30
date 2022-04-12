1b439c71840c2602036c5efa7716cf9391e3cf8c
apchapcomputing <apchapma@ncsu.edu>
2022-04-12 18:28:35 -0400

    finish day 29 countdown clock

--- 29 - Countdown Timer/scripts.js
+++ 29 - Countdown Timer/scripts.js
@@ -4,22 +4,20 @@ const endTime = document.querySelector('.display__end-time');
 const buttons = document.querySelectorAll('[data-time]');
 
 function timer(seconds) {
-  // clear any existing timers
   clearInterval(countdown);
 
   const now = Date.now();
-  const then = now + seconds * 1000;
+  const then = now + seconds * 1000; // convert seconds to milliseconds
   displayTimeLeft(seconds);
   displayEndTime(then);
 
   countdown = setInterval(() => {
     const secondsLeft = Math.round((then - Date.now()) / 1000);
-    // check if we should stop it!
-    if(secondsLeft < 0) {
+
+    if (secondsLeft < 0) {
       clearInterval(countdown);
       return;
     }
-    // display it
     displayTimeLeft(secondsLeft);
   }, 1000);
 }
@@ -27,7 +25,9 @@ function timer(seconds) {
 function displayTimeLeft(seconds) {
   const minutes = Math.floor(seconds / 60);
   const remainderSeconds = seconds % 60;
-  const display = `${minutes}:${remainderSeconds < 10 ? '0' : '' }${remainderSeconds}`;
+  const display = `${minutes}:${
+    remainderSeconds < 10 ? '0' : ''
+  }${remainderSeconds}`;
   document.title = display;
   timerDisplay.textContent = display;
 }
@@ -37,7 +37,9 @@ function displayEndTime(timestamp) {
   const hour = end.getHours();
   const adjustedHour = hour > 12 ? hour - 12 : hour;
   const minutes = end.getMinutes();
-  endTime.textContent = `Be Back At ${adjustedHour}:${minutes < 10 ? '0' : ''}${minutes}`;
+  endTime.textContent = `Be Back At ${adjustedHour}:${
+    minutes < 10 ? '0' : ''
+  }${minutes}`;
 }
 
 function startTimer() {
@@ -45,11 +47,11 @@ function startTimer() {
   timer(seconds);
 }
 
-buttons.forEach(button => button.addEventListener('click', startTimer));
-document.customForm.addEventListener('submit', function(e) {
+buttons.forEach((button) => button.addEventListener('click', startTimer));
+
+document.customForm.addEventListener('submit', function (e) {
   e.preventDefault();
   const mins = this.minutes.value;
-  console.log(mins);
   timer(mins * 60);
   this.reset();
 });
